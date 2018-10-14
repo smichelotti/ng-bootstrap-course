581fdca085fe275e069a0db4187c6846d0e72d37
Steve Michelotti <stemi@microsoft.com>
2018-10-14 13:56:32 -0400

    installing ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 // Third party imports
 import { NgxLoadingModule } from 'ngx-loading';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// App imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 import { EntryEditorComponent } from './entry-editor/entry-editor.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HttpClientModule } from '@angular/common/http';
 import { WorkoutsApiService } from './services/workouts-api.service';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     NgxLoadingModule.forRoot({}),
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [
     WorkoutsApiService
