388ce3003ac909c56dfb6ef165399b6255d3beab
Steve Michelotti <stemi@microsoft.com>
2018-10-20 17:47:46 -0400

    Accordion implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public stepComplete = 0;
+  @ViewChild('acc') acc: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  nextButton(step) {
+    this.stepComplete = step;
+    this.acc.toggle(`panel${this.stepComplete + 1}`);
+  }
+
+  beforeChange($event) {
+    if ($event.panelId === 'panel2' && this.stepComplete < 1) {
+      $event.preventDefault();
+    }
+  
+    if ($event.panelId === 'panel3' && this.stepComplete < 2) {
+      $event.preventDefault();
+    }
   }
 
 }
